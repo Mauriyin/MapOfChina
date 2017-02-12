99ae76fe002c19010caf4061f27fc6a08c584c72
Angelo.Yin <mauriyin0820@gmail.com>
2017-02-12 20:34:16 +0800

    add reset button

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ButtonClick(GameObject button)
+	{
+		int i;
+		for(i = 1; i <= 32; i++) {
+			string str = i.ToString();
+			GameObject now = GameObject.Find(str);
+			now.GetComponent<SpriteRenderer>().material.color = Color.white;
+		}
+		GameObject nows = GameObject.Find("Main Camera");
+		nows.GetComponent<DrawLineDemo>().enabled = false;
+	}
+}
